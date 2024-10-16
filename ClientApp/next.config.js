fddb41ea6eb65e5fb2d90ab26686bcca9ffa1305
Bryan Thomas <49354825+bryanjtc@users.noreply.github.com>
2024-10-16 12:42:48 -0500

    Update service worker configuration and dependencies

--- ClientApp/next.config.js
+++ ClientApp/next.config.js
@@ -19,7 +19,7 @@ module.exports = async (phase) => {
     const withSerwist = (await import("@serwist/next")).default({
       // Note: This is only an example. If you use Pages Router,
       // use something else that works, such as "service-worker/index.ts".
-      swSrc: "service-worker/index.ts",
+      swSrc: "service-worker/index.js",
       swDest: "public/sw.js",
     });
     return withSerwist(nextConfig);
